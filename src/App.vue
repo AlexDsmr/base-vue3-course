cc11dc426f58bfa78771073fa175a05dde1f40b2
AlexDsmr <ai.danilov.smr@gmail.com>
2022-04-03 22:10:10 +0400

    Fix create btn

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
     <div class="app">
-        <PostForm/>
-        <post-list v-bind:posts="posts"/>
+        <PostForm
+        @create="createPost"
+        />
+        <post-list 
+        :posts="posts"
+        />
     </div>
 </template>
 
@@ -20,20 +24,11 @@ export default {
                 {id: 3, title: 'JavaScript 3', body: 'Описание поста 3'},
                 {id: 4, title: 'JavaScript 4', body: 'Описание поста 4'}
             ],
-            title: '',
-            body: '',
         }
     },
     methods: {
-        createPost() {
-            const newPost = {
-                id: Date.now(),
-                title: this.title,
-                body: this.body,
-            }
-            this.posts.push(newPost);
-            this.title = '';
-            this.body = '';
+        createPost(post) {
+            this.posts.push(post);
         },
     }
 }
