046a6b50e5065c2c85b008e5f35c79edc738b3d3
AlexDsmr <ai.danilov.smr@gmail.com>
2022-04-03 19:24:00 +0400

    add posts and addPostForm

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
-    <div>
-    <div>
-        <button v-on:click="addLike">Like</button>
-        <button @click="addDislike">Dislike</button>
-    </div>
-     <div>Количество лайков: <strong>{{ likes }}</strong></div>
-     <div>Количество дизлайков: <strong>{{ dislikes }}</strong></div>
+    <div class="app">
+        <form>
+            <h4>Создание поста</h4>
+            <input class="input" type="text" placeholder="Название">
+            <input class="input" type="text" placeholder="Описание">
+            <button class="btn">Создать</button>
+        </form>
+        <div class="post" v-for="post in posts">
+            <div><strong>Название:</strong>{{ post.title }}</div>
+            <div><strong>Описание:</strong>{{ post.body }}</div>
+        </div>
     </div>
 </template>
 
@@ -13,21 +17,55 @@
 export default {
     data(){
         return{
-           likes: 0,
-           dislikes: 5,
+            posts: [
+                {id: 1, title: 'JavaScript', body: 'Описание поста'},
+                {id: 2, title: 'JavaScript 2', body: 'Описание поста 2'},
+                {id: 3, title: 'JavaScript 3', body: 'Описание поста 3'},
+                {id: 4, title: 'JavaScript 4', body: 'Описание поста 4'}
+            ]
         }
     },
     methods: {
-        addLike(){
-            this.likes += 1;
-        },
-        addDislike(){
-            this.dislikes += 1;
-        }
+
     }
 }
 </script>
 
 <style>
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
 
+.app {
+    padding: 20px;
+}
+
+.post{
+    padding: 15px;
+    border: 2px solid teal;
+    margin-top: 15px;
+}
+
+form {
+    display: flex;
+    flex-direction: column;
+}
+
+.btn {
+    margin-top: 15px;
+    align-self: flex-end;
+    padding: 10px 15px;
+    background: none;
+    color: teal;
+    border: 1px solid teal;
+}
+
+.input{
+    width: 100%;
+    border: 1px solid teal;
+    padding: 10px 15px;
+    margin-top: 15px;
+}
 </style>
