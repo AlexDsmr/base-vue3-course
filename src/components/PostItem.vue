2da8ad9a8abda8e5a99efc5dfffc10831e7ae201
AlexDsmr <ai.danilov.smr@gmail.com>
2022-04-03 22:43:06 +0400

    Feature add delete btn

--- src/components/PostItem.vue
+++ src/components/PostItem.vue
@@ -1,7 +1,13 @@
 <template>
     <div class="post" >
-        <div><strong>Название:</strong>{{ post.title }}</div>
-        <div><strong>Описание:</strong>{{ post.body }}</div>
+        <div>
+            <div><strong>Название:</strong>{{ post.title }}</div>
+            <div><strong>Описание:</strong>{{ post.body }}</div>
+        </div>
+        <div class="post__btns">
+            <button>Удалить</button>
+
+        </div>
     </div>
 </template>
 
@@ -17,5 +23,12 @@ export default {
 </script>
 
 <style scoped>
-
+.post{
+    padding: 15px;
+    border: 2px solid teal;
+    margin-top: 15px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
 </style>
