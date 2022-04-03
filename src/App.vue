87290f3ac44cb8d320e926ca6508957e35f94684
AlexDsmr <ai.danilov.smr@gmail.com>
2022-04-03 21:27:17 +0400

    decomposition of PostForm and PostList

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,17 @@
 <template>
     <div class="app">
-        <form @submit.prevent>
-            <h4>Создание поста</h4>
-            <input 
-            v-bind:value="title" 
-            @input="title = $event.target.value"
-            class="input" 
-            type="text" 
-            placeholder="Название">
-            <input 
-            v-bind:value="body"
-            @input="body = $event.target.value"
-            class="input"
-            type="text" 
-            placeholder="Описание">
-            <button 
-            class="btn" 
-            @click="createPost">Создать</button>
-        </form>
-        <div class="post" v-for="post in posts">
-            <div><strong>Название:</strong>{{ post.title }}</div>
-            <div><strong>Описание:</strong>{{ post.body }}</div>
-        </div>
+        <PostForm/>
+        <post-list v-bind:posts="posts"/>
     </div>
 </template>
 
 <script>
+import PostForm from "@/components/PostForm";
+import PostList from "@/components/Postlist";
 export default {
+    components:{
+      PostList, PostForm
+    },
     data(){
         return{
             posts: [
@@ -65,30 +50,5 @@ export default {
     padding: 20px;
 }
 
-.post{
-    padding: 15px;
-    border: 2px solid teal;
-    margin-top: 15px;
-}
 
-form {
-    display: flex;
-    flex-direction: column;
-}
-
-.btn {
-    margin-top: 15px;
-    align-self: flex-end;
-    padding: 10px 15px;
-    background: none;
-    color: teal;
-    border: 1px solid teal;
-}
-
-.input{
-    width: 100%;
-    border: 1px solid teal;
-    padding: 10px 15px;
-    margin-top: 15px;
-}
 </style>
