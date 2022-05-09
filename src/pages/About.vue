79450bce6ec6e2ecead917c76f1a35962f39e04d
AlexDsmr <ai.danilov.smr@gmail.com>
2022-05-09 13:38:54 +0400

    feat: add router and Navbar

---
+++ src/pages/About.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Приложение было создано в ходе обучения Vue.js</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
